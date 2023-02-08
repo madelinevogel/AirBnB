441a7b505204f76e04e82976d48849720b383d9f
Madeline Vogel <madmvogel@gmail.com>
2023-02-08 13:40:10 -0600

    add new/create action

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,4 +8,25 @@ class RoomsController < ApplicationController
     @room = Room.find_by(id: params[:id])
     render :show
   end
+
+  def new
+    @room = Room.new
+    render :new
+  end
+
+  def create
+    @room = Room.create(
+      address: params[:room][:address],
+      city: params[:room][:city],
+      state: params[:room][:state],
+      price: params[:room][:price],
+      description: params[:room][:description],
+      home_type: params[:room][:home_type],
+      room_type: params[:room][:room_type],
+      total_occupancy: params[:room][:total_occupancy],
+      total_bedrooms: params[:room][:total_bedrooms],
+      total_bathrooms: params[:room][:total_bathrooms],
+    )
+    redirect_to "/rooms"
+  end
 end
