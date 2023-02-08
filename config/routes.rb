3d88552f8db73b2ac245b0bba424911bb8c9d549
Matthew Taylor <mjt.inbox@gmail.com>
2023-02-08 11:19:42 -0700

    Added authentication as described in guide_rails_full_stack_authentication.md

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/signup" => "users#new"
+  post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
