7b9bd7d27ccf52134fe2f181544d6413560c6540
Matthew Taylor <mjt.inbox@gmail.com>
2023-02-09 11:53:21 -0700

    reservation controller create and index actions

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :rooms
+  resources :reservations
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
